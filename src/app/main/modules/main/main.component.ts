910a9e989b1455a630cb40d21a4d6544164eec1e
Alexandrov Vladimir <dev.alexandrovhub@gmail.com>
2020-06-25 09:57:43 +0300

    add responsive

--- src/app/main/modules/main/main.component.ts
+++ src/app/main/modules/main/main.component.ts
@@ -1,17 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
+import { MatDrawerMode } from '@angular/material/sidenav';
 
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
   styleUrls: ['./main.component.scss']
 })
-export class MainComponent implements OnInit {
+export class MainComponent implements OnInit, OnDestroy {
 
+  isOpen = false;
   expanded = false;
-
-  constructor() { }
+  mode: MatDrawerMode = 'side';
+  private unsubscribe$: Subject<void> = new Subject<void>();
+  constructor(
+    breakpointObserver: BreakpointObserver,
+  ) {
+    breakpointObserver.observe([Breakpoints.XSmall])
+      .pipe(takeUntil(this.unsubscribe$))
+      .subscribe(result => {
+        this.isOpen = !result.matches;
+        if (result.matches) {
+          this.mode = 'push';
+        } else {
+          this.mode = 'side';
+        }
+      });
+  }
 
   ngOnInit(): void {
+  }
+
+  ngOnDestroy() {
+    this.unsubscribe$.next();
+    this.unsubscribe$.complete();
   }
 
 }
--- src/app/main/shared/header/header.component.ts
+++ src/app/main/shared/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
+
+  @Output() toggleEvent = new EventEmitter();
 
   constructor() { }
 
